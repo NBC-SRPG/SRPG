7c6a15db7e6758bb358f0ddaa4a5209595c6bd3d
황문규 <155706789+GeCrabKani@users.noreply.github.com>
2024-03-08 17:57:50 +0900

    feat: ResourceManager 추가
    - Managers 추가
    - ResourceManager 추가
    - Resource 폴더에 Sounds, Prefabs, Sprites 폴더 추가

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.Diagnostics;
+public class Managers : MonoBehaviour
+{
+    private static Managers s_instance = null;
+    public static Managers Instance { get { return s_instance; } }
+
+    private static ResourceManager s_resourceManager = new ResourceManager();
+    public static ResourceManager Resource { get { Init(); return s_resourceManager; } }
+
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private static void Init()
+    {
+        if (s_instance == null)
+        {
+            // @Managers 오브젝트 찾기
+            GameObject go = GameObject.Find("@Managers");
+
+            // 없으면 생성
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+            }
+            
+            // Managers 컴포넌트 Get 하여 s_instance에 적용
+            if (go.TryGetComponent(out Managers managers))
+            {
+                s_instance = managers;
+            }
+            // 없으면 Add
+            else
+            {
+                s_instance = go.AddComponent<Managers>();
+            }
+
+            // 씬 넘어가도 유지 되도록 DontDestroy
+            DontDestroyOnLoad(go);
+            
+            // 들고있는 매니저들 Init
+            s_resourceManager.Init();
+
+            // 앱 프레임 60으로 고정
+            Application.targetFrameRate = 60;
+        }
+    }
+}
