54405d049d176250a01ce8d33e2d97e3712d0258
황문규 <155706789+GeCrabKani@users.noreply.github.com>
2024-03-13 19:52:22 +0900

    feat: Inventory 뼈대 코드 작성

---
+++ Assets/Scripts/UI/ItemSelectUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSelectUI : UIBase
+{
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/ItemUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemUI : UIBase
+{
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+
+    }
+}
