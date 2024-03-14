f85ffcf88ad28581d0040d7f6ad313bb3b06781e
jhwoo944 <tpfflaosej@gmail.com>
2024-03-14 10:29:56 +0900

    SRPG-40 Feat: TraitSO.cs 작성 및 스크립터블 오브젝트 생성
    TraitSO.cs 작성 및 스크립터블 오브젝트 생성

--- Assets/Scripts/Objects/CharacterTrait/TraitData/TraitSO.cs
+++ Assets/Scripts/Objects/CharacterTrait/TraitData/TraitSO.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TraitSO : MonoBehaviour
+[CreateAssetMenu(menuName = "TraitData", fileName = "Trait_")]
+public class TraitSO : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Develope")]
+    public string trait_Id;//캐릭터 식별자
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Trait_description")]
+    public string traitName;//특성 이름
+    public string traitDescription;//특성 설명
+
+    [Header("Image")]
+    public Sprite traitSprite;//특성 스프라이트(=썸네일or아이콘)
+
+    [Header("Trait_Status")]
+    public Dictionary<int, int> traitStatus = new Dictionary<int, int>();
+
+    //[Header("Trait_function")]
+
+
 }
