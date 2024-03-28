73664b1c80a78c11d724fc76aa2121d52c39203c
SeRDic0705 <serdic0705@gmail.com>
2024-03-28 16:50:49 +0900

    refactor: AccountData 타입 변경

--- Assets/Scripts/Objects/Accounts/AccountData.cs
+++ Assets/Scripts/Objects/Accounts/AccountData.cs
@@ -5,29 +5,29 @@ using UnityEngine.TextCore.Text;
 public class AccountData
 {
     public Dictionary<string, int> stageClearData { get; set; }
-    public Dictionary<int, Character> characterData { get; set; }
+    public Dictionary<int, CharacterGrowth> characterData { get; set; }
     public PlayerData playerData { get; set; }
     public Dictionary<int, bool> missionClearData { get; set; }
-    //public Dictionary<ItemData, int> inventory { get; set; }  Todo: 아이템 데이터 추가 시 활성화 필요
-    public Dictionary<int, string[]> friendData { get; set; }
+    //public Dictionary<int, int> inventory { get; set; }  Todo: 아이템 데이터 추가 시 활성화 필요
+    public Dictionary<string, string[]> friendData { get; set; }
     public VersionData versionData { get; set; }
     public int gachaPoint { get; set; }
 
     // Init �޼���
     public void Init(
         Dictionary<string, int> stageClearData,
-        Dictionary<int, Character> characterData,
+        Dictionary<int, CharacterGrowth> characterData,
         PlayerData playerData,
         Dictionary<int, bool> missionClearData,
-        //Dictionary<ItemData, int> inventory,
-        Dictionary<int, string[]> friendData
+        //Dictionary<int, int> inventory,
+        Dictionary<string, string[]> friendData
         )
     {
         this.stageClearData = stageClearData ?? new Dictionary<string, int>();
-        this.characterData = characterData ?? new Dictionary<int, Character>();
+        this.characterData = characterData ?? new Dictionary<int, CharacterGrowth>();
         this.playerData = playerData ?? new PlayerData();
         this.missionClearData = missionClearData ?? new Dictionary<int, bool>();
-        //this.inventory = inventory ?? new Dictionary<ItemData, int>();
-        this.friendData = friendData ?? new Dictionary<int, string[]>();
+        //this.inventory = inventory ?? new Dictionary<int, int>();
+        this.friendData = friendData ?? new Dictionary<string, string[]>();
     }
 }
