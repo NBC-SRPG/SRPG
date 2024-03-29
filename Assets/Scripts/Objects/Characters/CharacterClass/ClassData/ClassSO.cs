7ab3e8fe5854da70d75091be15e421dbaba81e8a
SeRDic0705 <serdic0705@gmail.com>
2024-03-29 17:32:20 +0900

    refactor: ClassSO
    - ClassSO 리팩토링
    
    #SRPG-40

--- Assets/Scripts/Objects/Characters/CharacterClass/ClassData/ClassSO.cs
+++ Assets/Scripts/Objects/Characters/CharacterClass/ClassData/ClassSO.cs
@@ -6,28 +6,13 @@ using static Constants;
 public class ClassSO : ScriptableObject
 {
     [Header("Develope")]
-    public int class_Id;//클래스 식별자
-    public BaseClass bassClass; //뿌리 클래스
+    public int id;      //클래스 식별자
+    public BaseClass baseClass;     //뿌리 클래스
 
     [Header("Class_description")]
-    public string className;//클래스 이름
-    public string classDescription;//클래스 설명
+    public string className;        //클래스 이름
+    public string classDescription;     //클래스 설명
 
-    [Header("Class_Status_basic")] //increase = 깡스탯 증가량(합연산). multiply = 배율(%) 증가량(곱연산)
-    public int increaseHealth;
-    public int increaseAtk;
-    public int increaseDef;
-    public int increasecMov;
-
-    public float multiplyHealth;
-    public float multiplyAtk;
-    public float multiplyDef;
-
-    [Header("Class_Status_addition")]
-    public float increasecCtr; //치명타 확률 +
-    public float increasecCtd; //치명타 피해 +
-    public float increaseInflictDamage; //주는 최종 피해 증가 +
-    public float reducedTakenDamage; //받는 최종 피해 감소 -
     
     [Header("Class_unique")]
     public int uniqueID;//클래스의 별도 고유 효과. 세부로직 미구현.
