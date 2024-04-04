51040704e353ee9c16d6cf6e281a469423b75aa6
chai227chai <chai227chai@naver.com>
2024-04-04 20:29:43 +0900

    test

--- Assets/0-Scripts/Managers/BattleManager.cs
+++ Assets/0-Scripts/Managers/BattleManager.cs
@@ -72,6 +72,11 @@ public class BattleManager
         //다른 클라이언트는 서버가 준 데미지를 받아옴
         int damage = attacker.Attack - victim.Defend;// 임시 데미지 계산식
 
+        if(damage < 0)
+        {
+            damage = 0;
+        }
+
         damage = (int)((float)damage * ExtraDmgbyAttribute(attacker, victim));
 
         return damage;
@@ -86,6 +91,11 @@ public class BattleManager
         //이후 서버가 데미지를 계산해서 모든 클라이언트에 전달
         //다른 클라이언트는 서버가 준 데미지를 받아옴
         int damage = figure - victim.Defend;// 임시 데미지 계산식
+
+        if (damage < 0)
+        {
+            damage = 0;
+        }
 
         damage = (int)((float)damage * ExtraDmgbyAttribute(attacker, victim));
 
