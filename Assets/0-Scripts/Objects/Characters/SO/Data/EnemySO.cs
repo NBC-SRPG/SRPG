61fb08d103a2479b6738cc0d1aaddce269d48184
SeRDic0705 <serdic0705@gmail.com>
2024-04-11 21:10:36 +0900

    refactor: enemySO 캐릭터SO로부터 분리

--- Assets/0-Scripts/Objects/Characters/SO/Data/EnemySO.cs
+++ Assets/0-Scripts/Objects/Characters/SO/Data/EnemySO.cs
@@ -1,10 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Constants;
 
 [CreateAssetMenu(menuName = "CharacterData/EnemyData", fileName = "Enemy_")]
-public class EnemySO : CharacterSO
+public class EnemySO
 {
+    public AttackMethod attackMethod;     //공격 타입
+    public ElementType elementType;      //캐릭터 속성
+    
+    [Header("Develope")]
+    public int id;      //캐릭터 식별자
+
+    [Header("Status")]      //캐릭터 능력치
+    public int hp;
+    public int atk;
+    public int def;
+    public int mov;
+    public int range;     //공격 사정거리(근거리의 경우 0으로)
+    public Faction faction;     //캐릭터 소속
+
+    public ExSkillSO exSkillSO;
+    public PassiveSkillSO passiveSkillSO;
+
+    [Header("Ability")]      //특성SO.
+    public AbilitySO abilityT1;
+    public AbilitySO abilityT2;
+    public AbilitySO abilityT3;
+
+    [Header("Class")]       //클래스SO.
+    public ClassSO basicClass;
+    public ClassSO superiorClass;
+
+
+    [Header("EquipList")]
+    public EquipSO weapon;
+    public EquipSO armor;
+
+    [Header("Animator")]
+    public string animatorName;
+
     [Header("EnemyType")]
     public bool hasSkill;
     public bool isElite;
