0810498bda4a8620bf6c5827553a6475e24fb750
SeRDic0705 <serdic0705@gmail.com>
2024-04-17 19:23:28 +0900

    refactor: 패시브 효과를 위한 계수 Dictionary로 변경
    이제 odin을 사용하여 Inspector창에 Dictionary를 표시할 수 있기 때문에 Dictionary로 변경

--- Assets/0-Scripts/Objects/Characters/Passive/PassiveSO.cs
+++ Assets/0-Scripts/Objects/Characters/Passive/PassiveSO.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
-public class PassiveSO : ScriptableObject
+public class PassiveSO : SerializedScriptableObject
 {
-    public List<int> coefficients;
+    public Dictionary<string, int> coefficients;
     public string reflection;
 }
