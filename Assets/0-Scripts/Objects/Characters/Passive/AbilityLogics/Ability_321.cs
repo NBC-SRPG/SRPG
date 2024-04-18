bda9eaa7b22b94486566736d44cd87e486b924a9
jhwoo944 <tpfflaosej@gmail.com>
2024-04-18 21:51:46 +0900

    Feat: Ability_321
     - 턴 종료 시 체력 회복 효과
     - 캐릭터 잃은 HP 불러오는 방법을 알 필요가 있음

--- Assets/0-Scripts/Objects/Characters/Passive/AbilityLogics/Ability_321.cs
+++ Assets/0-Scripts/Objects/Characters/Passive/AbilityLogics/Ability_321.cs
@@ -13,7 +13,8 @@ public class Ability_321: PassiveLogic
 
     public override void OnTurnEnd()// 턴이 끝날 때
     {
-        
+        character.health.HealHealthByInt(150);
+        //1. 캐릭터 잃은 HP 값을 어떻게 불러오는지
     }
 }
 
