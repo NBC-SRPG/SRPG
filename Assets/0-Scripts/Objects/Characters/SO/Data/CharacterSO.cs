f3b703018ef1203e89b9b978d8e9a417297cbb7b
SeRDic0705 <serdic0705@gmail.com>
2024-04-18 19:27:16 +0900

    feat: 캐릭터 일러스트 추가

--- Assets/0-Scripts/Objects/Characters/SO/Data/CharacterSO.cs
+++ Assets/0-Scripts/Objects/Characters/SO/Data/CharacterSO.cs
@@ -11,6 +11,9 @@ public class CharacterSO : ScriptableObject
     [Header("Develope")]
     public int id;      //캐릭터 식별자
 
+    [Header("Sprites")]
+    public Sprite standing;    //캐릭터 식별자
+
     [Header("Status")]      //캐릭터 능력치
     public int hp;
     public int atk;
