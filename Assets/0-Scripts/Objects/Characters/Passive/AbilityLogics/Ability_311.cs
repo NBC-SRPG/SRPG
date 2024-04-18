0ad6df1cadf97f397051a95953ad659ee0d8b3ca
jhwoo944 <tpfflaosej@gmail.com>
2024-04-18 21:19:12 +0900

    Feature: 각 어빌리티 클래스 파일 생성
     -

--- Assets/0-Scripts/Objects/Characters/Passive/AbilityLogics/Ability_311.cs
+++ Assets/0-Scripts/Objects/Characters/Passive/AbilityLogics/Ability_311.cs
@@ -12,16 +12,15 @@ public class Ability_311: PassiveLogic
 
     public override void OnStartAttack(CharacterBase enemy)// 공격 시작 시
     {
-        List<CharacterBuf> negativeBufList = enemy.curCharacterBufList.FindNegativeBufAll(); //대상이 보유한 디버프 갯수를 받아온다.
-        if (negativeBufList.Count > 0) //대상이 보유한 디버프 효과의 갯수가 1개 이상이면, 보너스 스탯(주는 피해+15%) 획득
+        if (enemy.curCharacterBufList.FindNegativeBufAll().Count > 0) //대상이 보유한 디버프 효과의 갯수가 1개 이상이면, 보너스 스탯(주는 피해+15%) 획득
         {
             stat.EnhancedDmg = 15;
             character.tempBonusStat.AddBonusStat(stat);
 
             /*
-            이 방식을 사용했을 때 찝찝한 점.
-            1. 
-
+            참고 사항
+            1. 이 방식을 사용하면 다수의 적을 공격할 때, 상태이상을 갖고 있는 적과 갖고 있지 않은 적이 섞여있어도 모두에게 15% 증가한 데미지를 가하게 됨.
+            2. 편성 중인 파티에 특정 캐릭터(여기서는 107. 시스)가 있는지 확인하는 로직 구현 필요.
              */
         }
         else
@@ -34,110 +33,39 @@ public class Ability_311: PassiveLogic
 
     public override void OnEndAttack(CharacterBase enemy)// 공격 종료 시
     {
-
+        character.tempBonusStat.RemoveBonusStat(stat);
     }
 
     public override void OnUseSkill(List<CharacterBase> targets)// 스킬 사용 시
     {
+        int bufCount = 0;
+        foreach (CharacterBase target2 in targets)
+        {
+            bufCount += target2.curCharacterBufList.FindNegativeBufAll().Count;
+        }
 
-    }
 
-    public override void OnSkillAttackSuccess(CharacterBase target, BattleKeyWords.Damage damage)// 스킬 적중 시
-    {
+        if (bufCount > 0) //대상이 보유한 디버프 효과의 갯수가 1개 이상이면, 보너스 스탯(주는 피해+15%) 획득
+        {
+            stat.EnhancedDmg = 15;
+            character.tempBonusStat.AddBonusStat(stat);
 
-    }
+            /*
+            참고 사항
+            1. 이 방식을 사용하면 다수의 적을 공격할 때, 상태이상을 갖고 있는 적과 갖고 있지 않은 적이 섞여있어도 모두에게 15% 증가한 데미지를 가하게 됨.
+            2. 편성 중인 파티에 특정 캐릭터(여기서는 107. 시스)가 있는지 확인하는 로직 구현 필요.
+             */
+        }
+        else
+        {
 
-    public override void OnSkillHealSuccess(CharacterBase target, BattleKeyWords.Damage heal)// 스킬로 체력 회복 시
-    {
+        }
 
     }
 
     public override void OnEndSkill(List<CharacterBase> target)// 스킬 사용 종료 시
     {
-
-    }
-
-    public override void AfterTakeAttacked(CharacterBase enemy)// 공격 받은 이후에
-    {
-
-    }
-
-    public override void OnTakeAttack(CharacterBase enemy)// 공격 받기 이전에
-    {
-
-    }
-
-    public override void OnTakeDamage(ref int damage, CharacterBase enemy = null,
-        BattleKeyWords.AttackDamageType damageType = BattleKeyWords.AttackDamageType.None,
-        Constants.ElementType characterAttribute = Constants.ElementType.None)// 데미지를 입을 때
-    {
-
-    }
-
-    public override void OnTakeHeal(ref int damage, CharacterBase enemy = null,
-        BattleKeyWords.AttackDamageType damageType = BattleKeyWords.AttackDamageType.None,
-        Constants.ElementType characterAttribute = Constants.ElementType.None)// 힐을 받을 때
-    {
-
-    }
-
-    public override void AfterTakeDamage(int damage, CharacterBase enemy = null,
-        BattleKeyWords.AttackDamageType damageType = BattleKeyWords.AttackDamageType.None,
-        Constants.ElementType characterAttribute = Constants.ElementType.None)// 데미지를 받은 이후에
-    {
-
-    }
-
-    public override void AfterTakeHeal(int heal, CharacterBase skillUser = null,
-        BattleKeyWords.AttackDamageType damageType = BattleKeyWords.AttackDamageType.None,
-        Constants.ElementType characterAttribute = Constants.ElementType.None)// 힐 받은 이후에
-    {
-
-    }
-
-    public override void OnStartMoving()// 이동 시
-    {
-
-    }
-
-    public override void OnEndMoving()// 이동 끝난 직후
-    {
-
-    }
-
-    public override void OnEndActing()// 행동이 끝난 뒤
-    {
-
-    }
-
-    public override void OnRoundEnd()
-    {
-
-    }
-
-    public override void OnTurnEnd()// 턴이 끝날 때
-    {
-
-    }
-
-    public override void OnKillEnemy(CharacterBase enemy, Constants.ElementType characterAttribute = Constants.ElementType.None)// 적 처치 시
-    {
-
-    }
-
-    public override void OnDieInBattle(CharacterBase killer)// 전투 중 사망 시
-    {
-
-    }
-
-    public override void OnDie()// 사망 시
-    {
-
-    }
-
-    public override void OnUpdate()// 실시간 판정
-    {
-
+        character.tempBonusStat.RemoveBonusStat(stat);
     }
 }
 
