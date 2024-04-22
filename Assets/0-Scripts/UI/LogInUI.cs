14397696a7e2a6d8622d9d333991c41de3aa7cd1
SeRDic0705 <serdic0705@gmail.com>
2024-04-22 23:31:39 +0900

    feat: 로그인 UI추가 & 경험치테이블추가

---
+++ Assets/0-Scripts/UI/LogInUI.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogInUI : UIBase
+{
+    private bool isDataLoaded = false;
+
+    private enum Texts
+    {
+        DownloadSizeText,
+        StatusText
+    }
+
+    private enum Buttons
+    {
+        DownloadButton,
+        CloseButton,
+        GoogleLogInbutton
+    }
+
+    private enum Images
+    {
+        ProgressBar
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void OnDestroy()
+    {
+        Database.OnLoadingProgressChanged -= UpdateProgress;
+    }
+
+    private void Update()
+    {
+        if (isDataLoaded && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+
+    private void Init()
+    {
+        Managers.UI.SetCanvas(gameObject);
+
+        BindText(typeof(Texts));
+        BindImage(typeof(Images));
+        BindButton(typeof(Buttons));
+
+        
+
+        Database.OnLoadingProgressChanged += UpdateProgress;
+
+        StartCoroutine(Managers.DB.DataLoad());
+        //StartCoroutine(LoadAllData());
+    }
+
+    private void UpdateProgress(float progress)
+    {
+        GetImage((int)Images.ProgressBar).fillAmount += progress;
+        int progressPercentage = Mathf.FloorToInt(GetImage((int)Images.ProgressBar).fillAmount * 100);
+        GetText((int)Texts.StatusText).text = $"로딩 중... ({progressPercentage}%)";
+
+        if (progressPercentage >= 100)
+        {
+            isDataLoaded = true;
+            GetText((int)Texts.StatusText).text = "로딩 완료! 화면을 클릭하여 시작하세요.";
+        }
+    }
+
+    /*
+    private IEnumerator LoadAllData()
+    {
+        string[] dataPaths = { "stageClearData", "characterData", "playerData", "friendData", "formationData", "versionData", "gachaPoint", "mailBox", "missionData" };
+        int totalDataCount = dataPaths.Length;
+        int loadedDataCount = 0;
+
+        foreach (var path in dataPaths)
+        {
+            yield return StartCoroutine(ReadDataFromFirebase(path, () =>
+            {
+                loadedDataCount++;
+                UpdateProgress(loadedDataCount, totalDataCount);
+                if (loadedDataCount == totalDataCount)
+                {
+                    isDataLoaded = true;
+
+                    GetText((int)Texts.StatusText).text = "로딩 완료! 화면을 클릭하여 시작하세요.";
+                }
+            }));
+        }
+    }
+
+    private void UpdateProgress(int loaded, int total)
+    {
+        float progress = (float)loaded / total;
+        GetImage((int)Images.ProgressBar).fillAmount = progress;
+        GetText((int)Texts.StatusText).text = $"로딩 중... ({progress * 100:F0}%)";
+    }
+
+    private IEnumerator ReadDataFromFirebase(string path, Action onComplete)
+    {
+        var task = Managers.DB.userDB.Child(path).GetValueAsync();
+        yield return new WaitUntil(() => task.IsCompleted);
+
+        if (task.Exception != null)
+        {
+            Debug.LogError(task.Exception);
+        }
+        else
+        {
+            onComplete?.Invoke();
+        }
+    }
+    */
+}
--- Assets/0-Scripts/Utility/Login.cs
+++ Assets/0-Scripts/Utility/Login.cs
@@ -9,12 +9,12 @@ using Firebase.Auth;
 public class Login : MonoBehaviour
 {
     private FirebaseAuth auth;
-    /*
+    
     void Awake()
     {
         PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
-            .RequestServerAuthCode(false /* Don't force refresh *///)     // 연결된 백엔드 서버 애플리케이션에 전달되고 OAuth 토큰으로 교환될 수 있도록 서버 인증 코드를 생성하도록 요청
-            /*.RequestIdToken()       // ID 토큰 생성을 요청(Firebase에서 플레이어를 식별하는 데 사용)
+            .RequestServerAuthCode(false /* Don't force refresh */)     // 연결된 백엔드 서버 애플리케이션에 전달되고 OAuth 토큰으로 교환될 수 있도록 서버 인증 코드를 생성하도록 요청
+            .RequestIdToken()       // ID 토큰 생성을 요청(Firebase에서 플레이어를 식별하는 데 사용)
             .Build();
 
         PlayGamesPlatform.InitializeInstance(config);
@@ -75,5 +75,5 @@ public class Login : MonoBehaviour
         });
     }
 
-    */
+
 }
