2dbfa18acef21adeeaf4de693d824ec632eac975
jhwoo944 <tpfflaosej@gmail.com>
2024-04-25 15:17:43 +0900

    Fix: 버프 로직 복구
    -테스트용으로 주석처리해둔 버프 로직 원래대로 되돌림

--- Assets/0-Scripts/Objects/Characters/CharacterInBattle/BufAndDeBuf/CharacterBufList.cs
+++ Assets/0-Scripts/Objects/Characters/CharacterInBattle/BufAndDeBuf/CharacterBufList.cs
@@ -108,8 +108,6 @@ public class CharacterBufList
                 if (buf.onlyOne)//한 캐릭터의 버프 리스트에서 한 번에 하나만 존재해야 하는 버프의 경우
                 {
                    
-                    /*
-
                     CharacterBuf originBuf = FindBuf(key);// 기존의 존재하던 버프를 찾아서
 
                     if (originBuf != null)
@@ -123,7 +121,6 @@ public class CharacterBufList
                             buf.DestoyBuf();
                         }
                     }
-                    */
                 
                 }
 
