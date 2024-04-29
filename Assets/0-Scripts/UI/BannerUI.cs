38aea4390d2438c8aa9873606bbb0df4d0076461
SeRDic0705 <serdic0705@gmail.com>
2024-04-29 19:58:16 +0900

    feat: 가챠 배너 및 이미지 추가

--- Assets/0-Scripts/UI/BannerUI.cs
+++ Assets/0-Scripts/UI/BannerUI.cs
@@ -2,7 +2,7 @@ public class BannerUI : UIBase
 {
     private enum Texts
     {
-        BannerText
+
     }
 
     private enum Images
@@ -21,14 +21,7 @@ public class BannerUI : UIBase
         BindImage(typeof(Images));
         BindButton(typeof(Buttons));
 
-        if (gachaSO.gachaType == Constants.GachaType.Common)
-        {
-            GetText((int)Texts.BannerText).text = "통상 계약";
-        }
-        else
-        {
-            GetText((int)Texts.BannerText).text = "픽업 계약";
-        }
+
         
         GetImage((int)Images.BannerImage).sprite = gachaSO.banner;
         GetButton((int)Buttons.BannerButton).onClick.AddListener(() => OnClickBannerButton(gachaSO));
