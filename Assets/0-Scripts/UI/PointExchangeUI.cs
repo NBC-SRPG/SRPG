7403330f3bef1f5be076bf65bb0d18f1d45d62bc
SeRDic0705 <serdic0705@gmail.com>
2024-04-29 23:45:25 +0900

    fix: 가챠포인트 교환UI에서 통상가챠를 참조하지않도록 수정

--- Assets/0-Scripts/UI/PointExchangeUI.cs
+++ Assets/0-Scripts/UI/PointExchangeUI.cs
@@ -9,7 +9,8 @@ public class PointExchangeUI : UIBase
 
     private enum Buttons
     {
-        BackImage
+        BackImage,
+        CloseButton
     }
 
     private enum GameObjects
@@ -34,13 +35,17 @@ public class PointExchangeUI : UIBase
 
         GetText((int)Texts.PointText).text = Managers.AccountData.playerData.gachaPoint.ToString();
         GetButton((int)Buttons.BackImage).onClick.AddListener(OnClickCloseButton);
+        GetButton((int)Buttons.CloseButton).onClick.AddListener(OnClickCloseButton);
 
         foreach (GachaSO gachaSO in Managers.UI.FindUI<GachaUI>().curGachaList)
         {
-            GameObject go = Managers.Resource.Instantiate(Managers.Resource.Load<GameObject>("Prefabs/UI/CharacterExchangeEntryUI"),
+            if (gachaSO.gachaType == Constants.GachaType.PickUp)
+            {
+                GameObject go = Managers.Resource.Instantiate(Managers.Resource.Load<GameObject>("Prefabs/UI/CharacterExchangeEntryUI"),
                 GetObject((int)GameObjects.Content).transform);
 
-            go.GetComponent<CharacterExchangeEntryUI>().Init(gachaSO);
+                go.GetComponent<CharacterExchangeEntryUI>().Init(gachaSO);
+            }
         }
 
         Managers.AccountData.playerData.OnGachaPointChanged += UpdateGachaPoint;
