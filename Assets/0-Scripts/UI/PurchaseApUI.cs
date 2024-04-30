008d6b8514f17f2121810a2835cda660870553cd
황문규 <155706789+GeCrabKani@users.noreply.github.com>
2024-04-30 17:27:03 +0900

    feat: Ap, 골드 구매 시 +, -, Max, Min 기능 추가

--- Assets/0-Scripts/UI/PurchaseApUI.cs
+++ Assets/0-Scripts/UI/PurchaseApUI.cs
@@ -1,12 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PurchaseApUI : UIBase
 {
+    private int purchaseCount = 1;
+    private int reduceDiamond = 50;
+    private int addAp = 100;
+
     private enum Texts
     {
-        
+        ApQuantity,
+        DiamondQuantity
     }
 
     private enum Buttons
@@ -14,12 +17,15 @@ public class PurchaseApUI : UIBase
         BackImage,
         CancelButton,
         CloseButton,
-        PurchaseButton
+        PurchaseButton,
+        IncreButton,
+        DecreButton,
+        MaxButton,
+        MinButton
     }
 
     public void Init()
     {
-
         BindText(typeof(Texts));
         BindButton(typeof(Buttons));
 
@@ -27,17 +33,56 @@ public class PurchaseApUI : UIBase
         GetButton((int)Buttons.CancelButton).onClick.AddListener(CloseUI);
         GetButton((int)Buttons.CloseButton).onClick.AddListener(CloseUI);
         GetButton((int)Buttons.PurchaseButton).onClick.AddListener(PurchaseAp);
+        GetButton((int)Buttons.IncreButton).onClick.AddListener(OnClickIncreButton);
+        GetButton((int)Buttons.DecreButton).onClick.AddListener(OnClickDecreButton);
+        GetButton((int)Buttons.MaxButton).onClick.AddListener(OnClickMaxButton);
+        GetButton((int)Buttons.MinButton).onClick.AddListener(OnClickMinButton);
+    }
+
+    private void OnClickIncreButton()
+    {
+        purchaseCount++;
+        TextUpdate();
+    }
+    private void OnClickDecreButton()
+    {
+        if (purchaseCount == 1)
+        {
+            return;
+        }
+        purchaseCount--;
+        TextUpdate();
+    }
+    private void OnClickMaxButton()
+    {
+        purchaseCount = Managers.AccountData.playerData.Diamond / reduceDiamond;
+        if (purchaseCount == 0)
+        {
+            purchaseCount = 1;
+        }
+        TextUpdate();
+    }
+    private void OnClickMinButton()
+    {
+        purchaseCount = 1;
+        TextUpdate();
+    }
+    
+    private void TextUpdate()
+    {
+        GetText((int)Texts.ApQuantity).text = (purchaseCount * addAp).ToString();
+        GetText((int)Texts.DiamondQuantity).text = (purchaseCount * reduceDiamond).ToString();
     }
 
     private void PurchaseAp()
     {
-        if (Managers.AccountData.playerData.ReduceDiamond(50) == false)
+        if (Managers.AccountData.playerData.ReduceDiamond(purchaseCount * reduceDiamond) == false)
         {
             Managers.UI.ShowUI<WarningUI>().Init("다이아가 부족합니다.");
 
             return;
         }
-        Managers.AccountData.playerData.AddAP(100);
+        Managers.AccountData.playerData.AddAP(purchaseCount * addAp);
 
         CloseUI();
     }
--- Assets/0-Scripts/UI/PurchaseGoldUI.cs
+++ Assets/0-Scripts/UI/PurchaseGoldUI.cs
@@ -1,12 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PurchaseGoldUI : UIBase
 {
+    private int purchaseCount = 1;
+    private int reduceDiamond = 50;
+    private int addGold = 100000;
+
     private enum Texts
     {
-        
+        GoldQuantity,
+        DiamondQuantity
     }
 
     private enum Buttons
@@ -14,7 +17,11 @@ public class PurchaseGoldUI : UIBase
         BackImage,
         CancelButton,
         CloseButton,
-        PurchaseButton
+        PurchaseButton,
+        IncreButton,
+        DecreButton,
+        MaxButton,
+        MinButton
     }
 
     public void Init()
@@ -27,16 +34,55 @@ public class PurchaseGoldUI : UIBase
         GetButton((int)Buttons.CancelButton).onClick.AddListener(CloseUI);
         GetButton((int)Buttons.CloseButton).onClick.AddListener(CloseUI);
         GetButton((int)Buttons.PurchaseButton).onClick.AddListener(PurchaseGold);
+        GetButton((int)Buttons.IncreButton).onClick.AddListener(OnClickIncreButton);
+        GetButton((int)Buttons.DecreButton).onClick.AddListener(OnClickDecreButton);
+        GetButton((int)Buttons.MaxButton).onClick.AddListener(OnClickMaxButton);
+        GetButton((int)Buttons.MinButton).onClick.AddListener(OnClickMinButton);
+    }
+
+    private void OnClickIncreButton()
+    {
+        purchaseCount++;
+        TextUpdate();
+    }
+    private void OnClickDecreButton()
+    {
+        if (purchaseCount == 1)
+        {
+            return;
+        }
+        purchaseCount--;
+        TextUpdate();
+    }
+    private void OnClickMaxButton()
+    {
+        purchaseCount = Managers.AccountData.playerData.Diamond / reduceDiamond;
+        if (purchaseCount == 0)
+        {
+            purchaseCount = 1;
+        }
+        TextUpdate();
+    }
+    private void OnClickMinButton()
+    {
+        purchaseCount = 1;
+        TextUpdate();
+    }
+
+    private void TextUpdate()
+    {
+        GetText((int)Texts.GoldQuantity).text = (purchaseCount * addGold).ToString();
+        GetText((int)Texts.DiamondQuantity).text = (purchaseCount * reduceDiamond).ToString();
     }
 
     private void PurchaseGold()
     {
-        if (Managers.AccountData.playerData.ReduceDiamond(50) == false)
+        if (Managers.AccountData.playerData.ReduceDiamond(purchaseCount * reduceDiamond) == false)
         {
             Managers.UI.ShowUI<WarningUI>().Init("다이아가 부족합니다.");
             return;
         }
-        Managers.AccountData.playerData.AddGold(100000);
+        Managers.AccountData.playerData.AddGold(purchaseCount * addGold);
 
         CloseUI();
     }
