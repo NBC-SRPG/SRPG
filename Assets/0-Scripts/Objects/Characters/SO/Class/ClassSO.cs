f1dac3a8052fd6e4afe108b5ce833c6b32d151cf
jhwoo944 <tpfflaosej@gmail.com>
2024-04-30 16:32:50 +0900

    Fix: 스킬 / 클래스 / 특성 툴팁에 [TextArea]적용, 줄바꿈 적용
     - 일부 스킬 툴팁을 일관성있게 수정

--- Assets/0-Scripts/Objects/Characters/SO/Class/ClassSO.cs
+++ Assets/0-Scripts/Objects/Characters/SO/Class/ClassSO.cs
@@ -11,6 +11,7 @@ public class ClassSO : PassiveSO
 
     [Header("Class_description")]
     public string className;        //클래스 이름
+    [TextArea]
     public string description;     //클래스 설명
     public Sprite icon;
 }
